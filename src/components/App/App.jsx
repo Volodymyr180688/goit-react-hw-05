e600dd64d7510dd38b250b631578adc3cc7893c3
Volodymyr180688 <shurik180688@gmail.com>
2024-05-23 23:16:00 +0300

    Homework #5

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom';
 import style from './App.module.css'
+import HomePage from '../../pages/HomePage/HomePage';
+import MoviesPage from '../../pages/MoviesPage/MoviesPage'
+// import MovieDetailsPage from '../../pages/MovieDetailsPage/MovieDetailsPage'
+import Navigation from '../Navigation/Navigation';
+// import MovieCast from '../MovieCast/MovieCast'
+// import MovieReviews from '../MovieReviews/MovieReviews'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-    </>
+  return (<div className={style.container}>
+    <Navigation />
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/movies' element={<MoviesPage />} />
+      {/* <Route path='/movies/:movieId' element={MovieDetailsPage}>
+        <Route path='cast' element={MovieCast} />
+        <Route path='reviews' element={MovieReviews} />
+      </Route> */}
+    </Routes>
+  </div>
   )
 }
 

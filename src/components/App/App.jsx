b9ee8f501abce6ee5daf41de44bb90a3513ee23e
Volodymyr180688 <shurik180688@gmail.com>
2024-05-25 14:35:05 +0300

    Homework

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,25 @@
 import { Route, Routes } from 'react-router-dom';
 import style from './App.module.css'
 import HomePage from '../../pages/HomePage/HomePage';
-import MoviesPage from '../../pages/MoviesPage/MoviesPage'
-// import MovieDetailsPage from '../../pages/MovieDetailsPage/MovieDetailsPage'
+import MoviesPage from '../../pages/MoviesPage/MoviesPage';
+import MovieDetailsPage from '../../pages/MovieDetailsPage/MovieDetailsPage';
 import Navigation from '../Navigation/Navigation';
-// import MovieCast from '../MovieCast/MovieCast'
-// import MovieReviews from '../MovieReviews/MovieReviews'
-
-
-
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
 function App() {
-
-  return (<div className={style.container}>
-    <Navigation />
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/movies' element={<MoviesPage />} />
-      {/* <Route path='/movies/:movieId' element={MovieDetailsPage}>
-        <Route path='cast' element={MovieCast} />
-        <Route path='reviews' element={MovieReviews} />
-      </Route> */}
-    </Routes>
-    
-  </div>
-  )
+  return (
+    <div className={style.container}>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+          {/* <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} /> */}
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
-import style from './MovieItem.module.css'
+import style from './MovieItem.module.css';
 
-export default function MovieItem({movie}) { 
+export default function MovieItem({ movie }) {
+    const { id, poster_path, title } = movie;
     const defaultImg = 'https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg';
+    
     return (
         <div>
-            <Link to={`/${movie.id}`} className={style.container}>
-                <img className={style.image}
-                    src={movie.poster_path 
-                        ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` 
-                        : defaultImg}
-                    alt="poster"
-                />
+            <Link to={`/movies/${id}`} className={style.container}>
+                <img 
+    className={style.image}
+    src={poster_path 
+        ? `https://image1.tmdb.org/t/p/w500/${poster_path}` 
+        : defaultImg}
+    alt={title}
+/>
             </Link>
         </div>
     );

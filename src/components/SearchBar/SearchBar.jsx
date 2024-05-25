0e404757b501e57a8c670b5237908831d712db6e
Volodymyr180688 <shurik180688@gmail.com>
2024-05-25 19:37:01 +0300

    фвв

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,7 +4,7 @@ import style from './SearchBar.module.css';
 export default function SearchBar({ onSubmit }) {
     return (
         <form onSubmit={onSubmit} className={style.form}>
-            <input type="text" name='movieName' placeholder='Enter the title to search' autoComplete="off" autoFocus className={style.input}/>
+            <input type="text" name='movieName' placeholder='Enter the title to search' autoComplete="off" autoFocus autoFocus pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$" className={style.input}/>
             <button type='submit' className={style.btn}>Search</button>
         </form>
     )
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,5 +1,5 @@
 import { useSearchParams, useLocation } from "react-router-dom";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { fetchSearchMovie } from '../../api-TMDB';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import Loader from '../../components/Loader/Loader';
@@ -7,24 +7,25 @@ import toast from 'react-hot-toast';
 import style from './MoviesPage.module.css';
 import MoviesList from "../../components/MoviesList/MoviesList";
 
-export default function MoviesPage() {
-    const location = useLocation();
+export default function MoviesPage() {    
     const [searchParams, setSearchParams] = useSearchParams();
     const movieName = searchParams.get('movieName') ?? '';
     const [moviesList, setMoviesList] = useState([]);
     const [loading, setLoading] = useState(false);
     const [formSubmitted, setFormSubmitted] = useState(false);
+    
+
+    
+
     useEffect(() => {
+        if (!movieName) return;
         setMoviesList([]);
         setLoading(true);
         const getMovieSearch = async (movieName) => {
             try {
-                setLoading(true);
                 const data = await fetchSearchMovie(movieName);
                 if (!data.results.length && formSubmitted) { 
-                    toast.error(
-                        'There are no movies with this request. Please, try again'
-                    );
+                    toast.error('There are no movies with this request. Please, try again');
                     return;
                 }
                 setMoviesList(data.results);
@@ -33,36 +34,29 @@ export default function MoviesPage() {
             } finally {
                 setLoading(false);
             }
-        }
+        };
         getMovieSearch(movieName);
     }, [movieName, formSubmitted]);
 
     const handleSubmit = e => {
         e.preventDefault();
         const searchForm = e.currentTarget;
-        const newMovieName = searchForm.elements.movieName.value;
-        if (!newMovieName.trim()) {
+        const newMovieName = searchForm.elements.movieName.value.trim();
+        if (!newMovieName) {
             toast.error('Please enter a keyword!');
             return;
         }
         setSearchParams({ movieName: newMovieName });
         searchForm.reset();
         setFormSubmitted(true);
-    };
 
-    useEffect(() => {
-        if (moviesList.length > 0) {
-            const state = { ...location.state, searchResults: moviesList };
-            window.history.replaceState(state, '', location.pathname);
-        }
-    }, [moviesList, location.state, location.pathname]);
+    }
 
     return (
         <div className={style.container}>
             <SearchBar onSubmit={handleSubmit} />
             {loading && <Loader />}
             <MoviesList movies={moviesList} />
-            
         </div>
-    )
+    );
 }
